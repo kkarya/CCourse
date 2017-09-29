9f541c6af19c7ca3ab4ef747d3eb68bcfc9b5c5a
Karina <31802020+kkarya@users.noreply.github.com>
2017-09-30 00:08:23 +0300

    Create task1.cpp

---
+++ Lesson_3 /HW/task1.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+
+
+int main()
+{
+	float a, b, c, d, x1, x2 = 0;
+	printf("Enter A, B, C: ");
+	scanf_s("%f, %f, %f", &a, &b, &c);
+	if (a == 0)
+	{
+		printf("A should not be zero\n");
+		return 0;
+	}
+	d = sqrt(b) - 4 * a * c;
+	printf("D = %f;\n", d);
+	if (d < 0)
+	{
+		printf("D < 0, no roots\n");
+		return 0;
+	}
+
+	x1 = ((-1) * b + sqrtf(d)) / (2 * a);
+	x2 = ((-1) * b - sqrtf(d)) / (2 * a);
+
+	printf("X1 = %.1f; X2 = %.1f;\n", x1, x2);
+
+    return 0;
+}
