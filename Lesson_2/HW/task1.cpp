a56f92c0a097d5ea1fa73053c745b60a43d8ee94
Karina <31802020+kkarya@users.noreply.github.com>
2017-09-30 10:39:31 +0300

    Create task1.cpp

---
+++ Lesson_2/HW/task1.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+int main () 
+{ 
+int my_var = 1; 
+int result = (my_var << 2) | 1; 
+printf ("% d \ n", результат);
+
+result = (my_var <<3)|7;
+printf("%d\n", result);
+
+result = (my_var << 4) | 1;
+printf("%d\n", result);
+
+result = (my_var << 6);
+printf("%d\n", result);
+
+result = (my_var <<10);
+printf("%d\n", result);
+getchar();
+return 0;
+}
