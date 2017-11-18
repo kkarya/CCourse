31ff649c363cc56390f3817817945c3202cfb693
Karina <31802020+kkarya@users.noreply.github.com>
2017-11-18 12:20:48 +0300

    Create task7.cpp

---
+++ Lesson_8/CW/task7.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+	srand(time(NULL));
+	int *vector = NULL;
+	int vector_size = 0;
+	scanf_s("%d", &vector_size);
+	vector = (int *)calloc(sizeof(int), vector_size);
+	
+	for (int i = 0; i < vector_size; i++)
+		vector[i] = rand() % 5;
+	
+	for (int i = 0; i < vector_size; i++)
+		printf("%d\n", vector[i]);
+	return 0;
+}
